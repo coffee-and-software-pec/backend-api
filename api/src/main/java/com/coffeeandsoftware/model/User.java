084f4e677b91b97b5b667c1ef742d4bff101c24e
Pedro Falcão <63296960+PedroFDiniz@users.noreply.github.com>
2023-05-11 17:29:26 -0300

    User Class

---
+++ api/src/main/java/com/coffeeandsoftware/model/User.java
@@ -0,0 +1,22 @@
+package com.coffeeandsoftware.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String pictureURL;
+}
+
