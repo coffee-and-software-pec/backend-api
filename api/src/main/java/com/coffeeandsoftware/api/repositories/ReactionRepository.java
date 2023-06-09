678f27fe6e54187af07a8e26517682183154ffbe
PedroFDiniz <63296960+PedroFDiniz@users.noreply.github.com>
2023-06-09 15:34:33 -0300

    Fixed all queries

--- api/src/main/java/com/coffeeandsoftware/api/repositories/ReactionRepository.java
+++ api/src/main/java/com/coffeeandsoftware/api/repositories/ReactionRepository.java
@@ -10,12 +10,12 @@ import com.coffeeandsoftware.api.model.Reaction;
 import com.coffeeandsoftware.api.util.ReactionPK;
 
 public interface ReactionRepository extends JpaRepository<Reaction,ReactionPK> {
-    @Query("SELECT r FROM Reaction r WHERE r.id.userId = :userId")
+    @Query("SELECT r FROM Reaction r WHERE r.author.u_id = :userId")
     List<Reaction> findAllByAuthor(@Param("userId") UUID userId);
 
-    @Query("SELECT r FROM Reaction r WHERE r.id.publicationId = :publicationId")
+    @Query("SELECT r FROM Reaction r WHERE r.r_publication.p_id = :publicationId")
     List<Reaction> findAllByPublication(@Param("publicationId") UUID publicationId);
 
-    @Query("SELECT r FROM Reaction r WHERE r.id.userId = :userId and r.id.publicationId = :publicationId")
+    @Query("SELECT r FROM Reaction r WHERE r.r_publication.p_id = :userId and r.author.u_id = :publicationId")
     Optional<Reaction> findByIds(@Param("userId") UUID userId, @Param("publicationId") UUID publicationId);
 }
