17110fda5d0d10f46a72370869a1328e420aa8fd
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-08-08 23:19:54 -0300

    feat: add initial implementation of activities

---
+++ api/src/main/java/com/coffeeandsoftware/api/controller/ActivityController.java
@@ -0,0 +1,35 @@
+package com.coffeeandsoftware.api.controller;
+
+import com.coffeeandsoftware.api.dto.ReturnDTO.ActivityDTO;
+import com.coffeeandsoftware.api.services.ActivityService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
+
+@RestController
+@RequestMapping("/activity")
+public class ActivityController {
+
+    @Autowired
+    ActivityService activityService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getActivitiesFromUser(@PathVariable String userId) {
+        try {
+            List<ActivityDTO> activityDTOList = activityService.getActivitiesFromUser(userId);
+            return new ResponseEntity<>(
+                    activityDTOList.stream().sorted(Comparator.comparing(ActivityDTO::getCreatedDate).reversed()),
+            HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
---
+++ api/src/main/java/com/coffeeandsoftware/api/services/ActivityService.java
@@ -0,0 +1,57 @@
+package com.coffeeandsoftware.api.services;
+
+import com.coffeeandsoftware.api.dto.ReturnDTO.ActivityDTO;
+import com.coffeeandsoftware.api.dto.ReturnDTO.ActivityType;
+import com.coffeeandsoftware.api.model.Comment;
+import com.coffeeandsoftware.api.model.Publication;
+import com.coffeeandsoftware.api.model.Reaction;
+import com.coffeeandsoftware.api.model.User;
+import com.coffeeandsoftware.api.repositories.PublicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ActivityService {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    PublicationService publicationService;
+
+    public List<ActivityDTO> getActivitiesFromUser(String userId) {
+        List<ActivityDTO> activityDTOList = new ArrayList<>();
+
+        List<Publication> publications = publicationService.getAllUserPublications(userId);
+        for (Publication publication: publications) {
+            for (Reaction reaction: publication.getReactions()) {
+                activityDTOList.add(
+                        new ActivityDTO(
+                                reaction.getAuthor().getU_name(),
+                                reaction.getAuthor().getPhotoURL(),
+                                ActivityType.LIKE,
+                                reaction.getReactionDate(),
+                                ""
+                        )
+                );
+            }
+            for (Comment comment: publication.getComments()) {
+                activityDTOList.add(
+                        new ActivityDTO(
+                                comment.getAuthor().getU_name(),
+                                comment.getAuthor().getPhotoURL(),
+                                ActivityType.COMMENT,
+                                comment.getCreation_date(),
+                                comment.getC_text()
+                        )
+                );
+            }
+        }
+
+        return activityDTOList;
+    }
+}
