9297bb380f4df9d8b9be3ddfe87a8dcd76ce8cc3
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-08-19 20:08:38 -0300

    feat(security): add cors in websecurityconfig

--- api/src/main/java/com/coffeeandsoftware/api/configuration/WebSecurityConfig.java
+++ api/src/main/java/com/coffeeandsoftware/api/configuration/WebSecurityConfig.java
@@ -6,8 +6,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,12 +27,28 @@ public class WebSecurityConfig {
             "/publication",
             "/comment/byPublication",
             "/publication/**",
-            "/review/**"
+            "/review/**",
+            "/tag",
+            "/tag/**",
+            "/user/getByEmail/**"
     };
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://after-class-frontend.vercel.app"));
+        configuration.setAllowedMethods(List.of("POST", "PUT", "PATCH", "GET", "DELETE", "OPTIONS", "HEAD"));
+        configuration.setAllowedHeaders(List.of("Authorization", "content-type", "xsrf-token", "origin"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors().and()
+//            .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
             .authorizeRequests()
             .antMatchers(HttpMethod.POST, POST_WHITE_LIST)
             .permitAll()
@@ -38,10 +58,11 @@ public class WebSecurityConfig {
             .hasRole("ADMIN")
             .anyRequest()
             .fullyAuthenticated()
-            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
             .and().oauth2ResourceServer().jwt()
             .and()
-            .and().cors().disable().csrf().disable();
+            .and().csrf().disable()
+//            .cors().configurationSource(corsConfigurationSource())
+            ;
 
         return http.build();
     }
